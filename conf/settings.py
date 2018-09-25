fcc82ffbd419f9f7a0a339c25549072618243299
zhongyu211 <allen.zhongyu@gmail.com>
2018-09-25 21:57:30 +0800

    add settings file

---
+++ conf/settings.py
@@ -0,0 +1,31 @@
+__author__ = "allenz"
+import os
+import sys
+
+import os
+import sys
+
+#log configuration
+LOG_FILE_NAME = 'merge.log'
+
+#db configuration
+DB_INFOS = {
+    "impala": {
+        "host": "ip-172-32-0-159.us-west-1.compute.internal",
+        "port": 21050,
+        "user": 'admin',
+        "password": None
+    },
+    "mysql": {
+        "host": "172.32.0.143",
+        "port": 3306,
+        "user": "root",
+        "password": "Viewfin2018@"
+    },
+    "redshift": {
+        "host": "viewfin.cw9jfoph5mfr.us-west-1.redshift.amazonaws.com",
+        "port": 5439,
+        "user": "root",
+        "password": "Viewfin2018!"
+    }
+}
---
+++ dbconn/__init__.py
@@ -0,0 +1,3 @@
+__author__ = "allenz"
+import os
+import sys
